5f1c300478f009d9a46935e831e6b1421f3289c3
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-01 23:27:16 -0500

    Change a few errors. Add some factors.

--- crm.rb
+++ crm.rb
@@ -60,20 +60,24 @@ class CRM
 
 
 
-	 def modify_contact
-    puts "Enter the first name of the contact you would like to modify"
-    contact_to_modify = gets.chomp
-    contact = Contact.find(id_to_modify)
-    puts "Are you sure? (y/n)"
-    answer = gets.chomp
-    if answer == "y"
-    	puts "What attribute would you like to change? 1) First name 2) Last name 3) Email 4) Notes"
-		attribute = gets.chomp.downcase
-		puts "What is the new attribute?"
-		new_value = gets.chomp
-	end
+# ____________________________________________ NOT DONE _________________
 
-  end
+	#  def modify_contact
+ #    puts "Enter the first name of the contact you would like to modify"
+ #    contact_to_modify = gets.chomp
+ #    contact = Contact.find(id_to_modify)
+ #    puts "Are you sure? (y/n)"
+ #    answer = gets.chomp
+ #    if answer == "y"
+ #    	puts "What attribute would you like to change? 1) First name 2) Last name 3) Email 4) Notes"
+	# 	attribute = gets.chomp.downcase
+	# 	puts "What is the new attribute?"
+	# 	new_value = gets.chomp
+	# end
+
+ #  end
+
+ # ____________________________________________ NOT DONE _________________
 
 
 	# def modify_contact
